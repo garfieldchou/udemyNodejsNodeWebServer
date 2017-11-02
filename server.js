749a78108d0eef59633b39e8cd844fe2a26c032b
garfield <shaudai0402@hotmail.com>
2017-11-02 10:46:33 +0800

    small challenge to inject data inside of another template

--- server.js
+++ server.js
@@ -7,13 +7,10 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-	// res.send('<h1>Hello Express!</h1>');
-	res.send({
-		name: 'Andrew',
-		likes: [
-			'Biking',
-			'Cities'
-		]
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Welcome to my websitre',
+		currentYear: new Date().getFullYear()
 	});
 });
 
